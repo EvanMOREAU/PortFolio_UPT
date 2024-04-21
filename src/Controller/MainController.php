f32dff064027890fa858c49d571fc1a6a00bf02e
unknown <evan.moreau@etik.com>
2024-04-21 17:25:57 +0200

    Update 3'

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,8 +20,32 @@ class MainController extends AbstractController
     public function index(StudiesRepository $studiesRepository, ProjectRepository $projectRepository, SkillsRepository $skillsRepository, LanguagesRepository $languagesRepository, HoursWorkedRepository $hoursWorkedRepository, HappyClientsRepository $happyClientRepository, DocumentCategoryRepository $documentCategoryRepository, DocumentationsRepository $documentationsRepository): Response
     {
         $fullUrl = $_SERVER['REQUEST_URI'];
-        $rss = simplexml_load_file('http://feeds.feedburner.com/symfony/blog');
+        $rss = simplexml_load_file('http://feeds.feedburner.com/symfony/events');
+        $rss_sources = [
+            'http://feeds.feedburner.com/symfony/blog',
+            'http://feeds.feedburner.com/symfony/events',
+            'http://feeds.feedburner.com/knpuniversity',
+            'https://www.youtube.com/feeds/videos.xml?channel_id=UCLdVmxwj9dQqM8tJJp2LYGw'
+        ];
 
+        $rss_items = [];
+
+        foreach ($rss_sources as $source) {
+            $rss = @simplexml_load_file($source);
+
+            if ($rss !== false && isset($rss->channel->item)) {
+                foreach ($rss->channel->item as $item) {
+                    $rss_items[] = [
+                        'title' => (string)$item->title,
+                        'link' => (string)$item->link,
+                        'description' => (string)$item->description,
+                        'pubDate' => (string)$item->pubDate,
+                    ];
+                }
+            }
+        }
+        dump($rss_items);
+        //http://feeds.feedburner.com/symfony/blog
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             'url'             => $fullUrl,
@@ -33,7 +57,7 @@ class MainController extends AbstractController
             'languages' => $languagesRepository->findAll(),
             'hour' => $hoursWorkedRepository->findOneBy(array(), array('id' => 'ASC')),
             'happy' => $happyClientRepository->findOneBy(array(), array('id' => 'ASC')),
-            'rss_items' => $rss->channel->item ?? [], // Passer les éléments du flux RSS à Twig
+            'rss_items' => $rss_items,
         ]);
     }
 }
