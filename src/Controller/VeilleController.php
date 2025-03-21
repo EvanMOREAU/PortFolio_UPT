b2317a8edcc91458bd0f06fd1a05d00d665d43a4
ERIC DE BEL AIR Cylaos ICT <evan.moreau@etik.com>
2025-03-21 16:43:43 +0100

    correction

--- src/Controller/VeilleController.php
+++ src/Controller/VeilleController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VeilleController extends AbstractController
 {
     #[Route('/veille', name: 'app_veille')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $rss = simplexml_load_file('http://feeds.feedburner.com/symfony/events');
         $rss_sources = [
@@ -34,6 +38,17 @@ class VeilleController extends AbstractController
                     ];
                 }
             }
+        }
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_main', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('veille/index.html.twig', [
